6389ee287e357c6889bbe3ab4225e096c0c3113b
Chukwudi Nwachukwu <theichux@gmail.com>
2018-06-19 21:14:03 +0100

    Improvements done
    1. generates random 256bits for test file
    2. now works with pycrypto library

--- test_working_parts.py
+++ test_working_parts.py
@@ -1,18 +1,25 @@
+import Crypto.Random.random as randomize
 import json
+import logging
 import unittest
+import inspect
 
 import aes
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class BodyTest(unittest.TestCase):
     def setUp(self):
-        self.key = '7O86CFBE1563322983CB3515BF62B2652B5495BED15B4D450908B6B223FEE409D512CCB0DC2FB1C891E6329D2143418E'
+        self.key = str(randomize.getrandbits(256))
         self.nbits = 256
 
     def test_encrypt(self):
         data = json.dumps({'a': 1})
         response = aes.encrypt(data, self.key, self.nbits)
-        
+
+        logger.info(f"key, {self.key}, generated for function; {inspect.currentframe().f_code.co_name}")
         self.assertIsNotNone(response)
 
     def test_decrypt(self):
@@ -20,6 +27,8 @@ class BodyTest(unittest.TestCase):
 
         response = aes.encrypt(data, self.key, self.nbits)
         response = aes.decrypt(response, self.key, self.nbits)
+
+        logger.info(f"key, {self.key}, generated for function; {inspect.currentframe().f_code.co_name}")
         self.assertEqual(response, data)
 
 
